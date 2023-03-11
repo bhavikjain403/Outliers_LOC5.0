1c51f0d2c92babedf958818b81837f8e4b555db2
Bhavik Jain <bhavikjain403@gmail.com>
2023-03-11 12:59:35 +0530

    Minor change

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/login', authRoutes);
+app.use('/api', authRoutes);
 
 // app.use(middleware.unknownEndpointHandler);
 app.use(middleware.errorHandler);
