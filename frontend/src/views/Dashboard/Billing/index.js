7b44788ea438f46b9ca6cccd514fa8e60f1a1ee7
fibre-ether <fire.breather@rediff.com>
2023-03-11 16:02:13 +0530

    Create dashboard somewhat

--- frontend/src/views/Dashboard/Billing/index.js
+++ frontend/src/views/Dashboard/Billing/index.js
@@ -1,9 +1,30 @@
 // Chakra imports
-import { Box, Flex, Grid, Icon } from "@chakra-ui/react";
+import {
+  Box,
+  Checkbox,
+  CheckboxGroup,
+  Flex,
+  Grid,
+  Icon,
+  Stack,
+  Text,
+  useCheckbox,
+  useCheckboxGroup,
+  chakra,
+  RadioGroup,
+  Radio,
+  Spacer,
+  Input,
+  HStack,
+  Button,
+  Divider,
+  VStack,
+} from "@chakra-ui/react";
 // Assets
 import BackgroundCard1 from "assets/img/BackgroundCard1.png";
+import Card from "components/Card/Card";
 import { MastercardIcon, VisaIcon } from "components/Icons/Icons";
-import React from "react";
+import React, { useEffect } from "react";
 import { FaPaypal, FaWallet } from "react-icons/fa";
 import { RiMastercardFill } from "react-icons/ri";
 import {
@@ -20,76 +41,55 @@ import PaymentStatistics from "./components/PaymentStatistics";
 import Transactions from "./components/Transactions";
 
 function Billing() {
+  const [value, setValue] = React.useState("1");
   return (
-    <Flex direction='column' pt={{ base: "120px", md: "75px" }}>
-      <Grid templateColumns={{ sm: "1fr", lg: "2fr 1.2fr" }} templateRows='1fr'>
-        <Box>
-          <Grid
-            templateColumns={{
-              sm: "1fr",
-              md: "1fr 1fr",
-              xl: "1fr 1fr 1fr 1fr",
-            }}
-            templateRows={{ sm: "auto auto auto", md: "1fr auto", xl: "1fr" }}
-            gap='26px'>
-            <CreditCard
-              backgroundImage={BackgroundCard1}
-              title={"Purity UI"}
-              number={"7812 2139 0823 XXXX"}
-              validity={{
-                name: "VALID THRU",
-                data: "05/24",
-              }}
-              cvv={{
-                name: "CVV",
-                code: "09x",
-              }}
-              icon={
-                <Icon
-                  as={RiMastercardFill}
-                  w='48px'
-                  h='auto'
-                  color='gray.400'
-                />
-              }
-            />
-            <PaymentStatistics
-              icon={<Icon h={"24px"} w={"24px"} color='white' as={FaWallet} />}
-              title={"Salary"}
-              description={"Belong interactive"}
-              amount={2000}
-            />
-            <PaymentStatistics
-              icon={<Icon h={"24px"} w={"24px"} color='white' as={FaPaypal} />}
-              title={"Paypal"}
-              description={"Freelance Payment"}
-              amount={4550}
-            />
-          </Grid>
-          <PaymentMethod
-            title={"Payment Method"}
-            mastercard={{
-              icon: <MastercardIcon w='100%' h='100%' />,
-              number: "7812 2139 0823 XXXX",
-            }}
-            visa={{
-              icon: <VisaIcon w='100%' h='100%' />,
-              number: "7812 2139 0823 XXXX",
-            }}
-          />
-        </Box>
-        <Invoices title={"Invoices"} data={invoicesData} />
-      </Grid>
-      <Grid templateColumns={{ sm: "1fr", lg: "1.6fr 1.2fr" }}>
-        <BillingInformation title={"Billing Information"} data={billingData} />
-        <Transactions
-          title={"Your Transactions"}
-          date={"23 - 30 March"}
-          newestTransactions={newestTransactions}
-          olderTransactions={olderTransactions}
-        />
-      </Grid>
+    <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
+      <Card mb={"1em"}>
+        <Flex direction="column">
+          <Box>
+            <Title>Coupon Type</Title>
+            <RadioGroup onChange={setValue} value={value}>
+              <Radio value="1">Static Coupon</Radio>
+              <Stack direction="column">
+                <Radio value="2">Dynamic Coupon</Radio>
+              </Stack>
+            </RadioGroup>
+          </Box>
+          <Divider marginY={5} borderColor="#fff" />
+          <Box>
+            <Text fontSize="xl" fontWeight="bold">
+              Discount Code
+            </Text>
+            <HStack marginBottom={2}>
+              <Input></Input>
+              <Button rounded={"md"}>Generate</Button>
+            </HStack>
+            <Text fontSize={15} textColor="gray">
+              Customers must enter this code at checkout
+            </Text>
+          </Box>
+        </Flex>
+      </Card>
+      <Card>
+        <Flex direction={"column"}>
+            <Title>Value</Title>
+            <HStack w={"100%"} >
+              <Button rounded={"md"} px={10}>Percentage</Button>
+              <Button rounded={"md"} px={10}>Fixed Amount</Button>
+              <Input></Input>
+            </HStack>
+            <Title></Title>
+        </Flex >
+      </Card>
     </Flex>
+  );
+}
+
+function Title(props) {
+  return (
+    <Text fontSize="xl" fontWeight="bold" mb={5}>
+      {props.children}
+    </Text>
   );
 }
 
