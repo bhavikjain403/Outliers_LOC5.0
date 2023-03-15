7b03f93533c16a42968ec99e62c896b3416991ac
fibre-ether <fire.breather@rediff.com>
2023-03-15 13:05:36 +0530

    fix protected route

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { useAuth } from "./auth-context/auth.context";
-import { useHistory, Outlet } from "react-router-dom";
+import { useHistory, Redirect } from "react-router-dom";
 import { Route } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export const ProtectedRoute = ({ ...rest }) => {
   const history = useHistory();
-  let { user } = useAuth();
-  if (!user || !user.token || user.token === "") {
-    history.push("/auth/signin");
+  let { user, setUser } = useAuth();
+  const localUser = JSON.parse(localStorage.getItem("user"))?.data?.user
+  // console.log(localUser?.data?.user, user)
+
+  if (!user & localUser) {
+    setUser(localUser)
+  }
+
+  if ((!user || !user.token || user.token === "") & !localUser) {
+    // history.push("/auth/signin");
     toast.error("You must be logged in");
+    return <Redirect to="/login" {...rest} />;
   } else {
     return <Route {...rest} />;
   }
